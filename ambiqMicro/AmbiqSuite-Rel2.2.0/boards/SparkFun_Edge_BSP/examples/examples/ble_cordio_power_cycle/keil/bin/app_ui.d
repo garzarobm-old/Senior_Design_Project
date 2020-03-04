c63303566d0c6f105171f3ab49912e163e02034b
Miguel <Garzarobm@utexas.edu>
2020-03-03 23:32:57 -0600

    added seniorFit from a rendition of ble_freetos_fit_lp

---
+++ ambiqMicro/AmbiqSuite-Rel2.2.0/boards/SparkFun_Edge_BSP/examples/examples/ble_cordio_power_cycle/keil/bin/app_ui.d
@@ -0,0 +1,8 @@
+.\bin\app_ui.o: ../../../../../third_party/exactle/sw/apps/app/generic/app_ui.c
+.\bin\app_ui.o: ../../../../../third_party/exactle/ws-core/sw/wsf/ambiq/wsf_types.h
+.\bin\app_ui.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\bin\app_ui.o: ../../../../../third_party/exactle/ws-core/sw/wsf/include/wsf_os.h
+.\bin\app_ui.o: ../../../../../third_party/exactle/ws-core/sw/wsf/ambiq/wsf_os_int.h
+.\bin\app_ui.o: ../../../../../third_party/exactle/ws-core/sw/wsf/include/wsf_queue.h
+.\bin\app_ui.o: ../../../../../third_party/exactle/ws-core/sw/wsf/ambiq/wsf_trace.h
+.\bin\app_ui.o: ../../../../../third_party/exactle/sw/apps/app/include/app_ui.h
---
+++ ambiqMicro/AmbiqSuite-Rel2.2.0/boards/SparkFun_Edge_BSP/examples/examples/ble_cordio_power_cycle/keil/bin/wsf_math.d
@@ -0,0 +1,5 @@
+.\bin\wsf_math.o: ../../../../../third_party/exactle/ws-core/sw/wsf/ambiq/wsf_math.c
+.\bin\wsf_math.o: ../../../../../third_party/exactle/ws-core/sw/wsf/include/wsf_math.h
+.\bin\wsf_math.o: ../../../../../third_party/exactle/ws-core/sw/wsf/ambiq/wsf_types.h
+.\bin\wsf_math.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\bin\wsf_math.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\string.h
