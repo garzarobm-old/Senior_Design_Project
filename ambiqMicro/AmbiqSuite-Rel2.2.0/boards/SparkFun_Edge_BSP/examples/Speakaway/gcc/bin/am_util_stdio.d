3824454f6b0480f55fcb883b4e74be0e153ff94b
Miguel <Garzarobm@utexas.edu>
2020-03-25 18:38:04 -0500

    added functionality of seniorFit to SpeakAway

---
+++ ambiqMicro/AmbiqSuite-Rel2.2.0/boards/SparkFun_Edge_BSP/examples/Speakaway/gcc/bin/am_util_stdio.d
@@ -0,0 +1,4 @@
+bin/am_util_stdio.o: ../src/utils/am_util_stdio.c \
+ ../src/utils/am_util_stdio.h
+
+../src/utils/am_util_stdio.h:
---
+++ ambiqMicro/AmbiqSuite-Rel2.2.0/boards/SparkFun_Edge_BSP/examples/Speakaway/gcc/bin/wsf_buf.d
@@ -0,0 +1,28 @@
+bin/wsf_buf.o: ../src/third_party/exactle/ws-core/sw/wsf/common/wsf_buf.c \
+ ../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_types.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/include/wsf_buf.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_assert.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/include/wsf_math.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_trace.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_cs.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/include/wsf_os.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_os_int.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/include/wsf_queue.h
+
+../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_types.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/include/wsf_buf.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_assert.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/include/wsf_math.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_trace.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_cs.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/include/wsf_os.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_os_int.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/include/wsf_queue.h:
