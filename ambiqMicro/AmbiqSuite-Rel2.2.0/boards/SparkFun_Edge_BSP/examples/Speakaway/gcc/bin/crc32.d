5563e4177fdb04626d49ecb21a2bbd60025bce23
Miguel <Garzarobm@utexas.edu>
2020-03-26 15:03:14 -0500

    added untracked files

---
+++ ambiqMicro/AmbiqSuite-Rel2.2.0/boards/SparkFun_Edge_BSP/examples/Speakaway/gcc/bin/crc32.d
@@ -0,0 +1,7 @@
+bin/crc32.o: ../src/third_party/exactle/ws-core/sw/util/crc32.c \
+ ../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_types.h \
+ ../src/third_party/exactle/ws-core/sw/util/crc32.h
+
+../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_types.h:
+
+../src/third_party/exactle/ws-core/sw/util/crc32.h:
---
+++ ambiqMicro/AmbiqSuite-Rel2.2.0/boards/SparkFun_Edge_BSP/examples/Speakaway/gcc/bin/hidapp_main.d
@@ -0,0 +1,113 @@
+bin/hidapp_main.o: \
+ ../src/third_party/exactle/sw/apps/hidapp/hidapp_main.c \
+ ../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_types.h \
+ ../src/third_party/exactle/ws-core/sw/util/bstream.h \
+ ../src/third_party/exactle/ws-core/sw/util/bda.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/include/wsf_msg.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/include/wsf_queue.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/include/wsf_os.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_os_int.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_trace.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_assert.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/include/wsf_os.h \
+ ../src/third_party/exactle/sw/stack/include/hci_api.h \
+ ../src/third_party/exactle/ws-core/include/hci_defs.h \
+ ../src/third_party/exactle/ws-core/sw/util/bda.h \
+ ../src/third_party/exactle/sw/stack/include/dm_api.h \
+ ../src/third_party/exactle/sw/stack/include/hci_api.h \
+ ../src/third_party/exactle/sw/stack/cfg/cfg_stack.h \
+ ../src/third_party/exactle/sw/stack/include/smp_defs.h \
+ ../src/third_party/exactle/sw/sec/include/sec_api.h \
+ ../src/third_party/exactle/sw/stack/include/att_api.h \
+ ../src/third_party/exactle/ws-core/sw/wsf/include/wsf_timer.h \
+ ../src/third_party/exactle/sw/stack/include/att_defs.h \
+ ../src/third_party/exactle/sw/stack/include/att_uuid.h \
+ ../src/third_party/exactle/sw/stack/include/dm_api.h \
+ ../src/third_party/exactle/sw/stack/include/smp_api.h \
+ ../src/third_party/exactle/sw/apps/app/include/app_cfg.h \
+ ../src/third_party/exactle/sw/apps/app/include/app_api.h \
+ ../src/third_party/exactle/sw/apps/app/include/app_db.h \
+ ../src/third_party/exactle/sw/apps/app/include/app_db.h \
+ ../src/third_party/exactle/sw/apps/app/include/app_ui.h \
+ ../src/third_party/exactle/sw/services/svc_ch.h \
+ ../src/third_party/exactle/sw/services/svc_hid.h \
+ ../src/third_party/exactle/sw/services/svc_core.h \
+ ../src/third_party/exactle/sw/services/svc_dis.h \
+ ../src/third_party/exactle/sw/services/svc_batt.h \
+ ../src/third_party/exactle/sw/profiles/bas/bas_api.h \
+ ../src/third_party/exactle/sw/profiles/hid/hid_api.h \
+ ../src/third_party/exactle/sw/apps/hidapp/hidapp_api.h
+
+../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_types.h:
+
+../src/third_party/exactle/ws-core/sw/util/bstream.h:
+
+../src/third_party/exactle/ws-core/sw/util/bda.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/include/wsf_msg.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/include/wsf_queue.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/include/wsf_os.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_os_int.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_trace.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/ambiq/wsf_assert.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/include/wsf_os.h:
+
+../src/third_party/exactle/sw/stack/include/hci_api.h:
+
+../src/third_party/exactle/ws-core/include/hci_defs.h:
+
+../src/third_party/exactle/ws-core/sw/util/bda.h:
+
+../src/third_party/exactle/sw/stack/include/dm_api.h:
+
+../src/third_party/exactle/sw/stack/include/hci_api.h:
+
+../src/third_party/exactle/sw/stack/cfg/cfg_stack.h:
+
+../src/third_party/exactle/sw/stack/include/smp_defs.h:
+
+../src/third_party/exactle/sw/sec/include/sec_api.h:
+
+../src/third_party/exactle/sw/stack/include/att_api.h:
+
+../src/third_party/exactle/ws-core/sw/wsf/include/wsf_timer.h:
+
+../src/third_party/exactle/sw/stack/include/att_defs.h:
+
+../src/third_party/exactle/sw/stack/include/att_uuid.h:
+
+../src/third_party/exactle/sw/stack/include/dm_api.h:
+
+../src/third_party/exactle/sw/stack/include/smp_api.h:
+
+../src/third_party/exactle/sw/apps/app/include/app_cfg.h:
+
+../src/third_party/exactle/sw/apps/app/include/app_api.h:
+
+../src/third_party/exactle/sw/apps/app/include/app_db.h:
+
+../src/third_party/exactle/sw/apps/app/include/app_db.h:
+
+../src/third_party/exactle/sw/apps/app/include/app_ui.h:
+
+../src/third_party/exactle/sw/services/svc_ch.h:
+
+../src/third_party/exactle/sw/services/svc_hid.h:
+
+../src/third_party/exactle/sw/services/svc_core.h:
+
+../src/third_party/exactle/sw/services/svc_dis.h:
+
+../src/third_party/exactle/sw/services/svc_batt.h:
+
+../src/third_party/exactle/sw/profiles/bas/bas_api.h:
+
+../src/third_party/exactle/sw/profiles/hid/hid_api.h:
+
+../src/third_party/exactle/sw/apps/hidapp/hidapp_api.h:
